fb178b462208eaa6e78797a7ee9a73b11e0f4e40
Zetaphor <zetaphor@hey.com>
2023-09-06 09:36:15 -0300

    Controlnet coco segmentation working

--- image_test/text2img.py
+++ image_test/text2img.py
@@ -22,14 +22,14 @@ async def generate_image_from_text(input_image_path, output_image_path, positive
             "controlnet": {
                 "args": [
                     {
-                        "enabled": True,
-                        "module": "none",
+                        "module": "seg_ofcoco",
+                        "alias": True,
                         "model": "controlnet11Models_seg [ab613144]",
                         "image": encoded_image,
                         # "weight": 1.0,
                         # "resize_mode": 1,
                         # "lowvram": False,
-                        # "processor_res": 64,
+                        "processor_res": 1024,
                         # "threshold_a": 64,
                         # "threshold_b": 64,
                         # "guidance_start": 0.0,
@@ -48,12 +48,18 @@ async def generate_image_from_text(input_image_path, output_image_path, positive
                 raise ValueError(f"Request failed with status {response.status}")
             r = await response.json()
 
+    print('Images', len(r['images']))
     image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
     image.save(output_image_path)
+
+    if len(r['images']) == 2:
+        image = Image.open(io.BytesIO(base64.b64decode(r['images'][1])))
+        image.save('second_' + output_image_path)
     return output_image_path
+
 
 positive = 'illustration of a grassy field, masterpiece, trending on deviantart, (disney style:1.2), clouds'
 negative = 'frame, people, humans, characters'
-result = asyncio.run(generate_image_from_text('mountain-segmentation.png', 'controlnetoutput.png', positive, negative))
+result = asyncio.run(generate_image_from_text('mountain.jpg', 'controlnetoutput.png', positive, negative))
 print(result)
 
