be4b8cdd697b8d9026c45032c04ab556735c2c6d
Zetaphor <zetaphor@hey.com>
2023-09-06 00:10:13 -0300

    Added SD txt2img ControlNet script

---
+++ image_test/text2img.py
@@ -0,0 +1,59 @@
+import io
+import cv2
+import base64
+import aiohttp
+from PIL import Image
+import asyncio
+
+async def generate_image_from_text(input_image_path, output_image_path, positive_prompt, negative_prompt=""):
+    API_URL = "http://127.0.0.1:7860"
+
+    img = cv2.imread(input_image_path)
+    retval, bytes = cv2.imencode('.png', img)
+    encoded_image = base64.b64encode(bytes).decode('utf-8')
+
+    payload = {
+        "prompt": positive_prompt,
+        "negative_prompt": negative_prompt,
+        "batch_size": 1,
+        "steps": 20,
+        "cfg_scale": 7,
+        "alwayson_scripts": {
+            "controlnet": {
+                "args": [
+                    {
+                        "enabled": True,
+                        "module": "none",
+                        "model": "controlnet11Models_seg [ab613144]",
+                        "image": encoded_image,
+                        # "weight": 1.0,
+                        # "resize_mode": 1,
+                        # "lowvram": False,
+                        # "processor_res": 64,
+                        # "threshold_a": 64,
+                        # "threshold_b": 64,
+                        # "guidance_start": 0.0,
+                        # "guidance_end": 1.0,
+                        # "control_mode": 0,
+                        # "pixel_perfect": False
+                    }
+                ]
+            }
+        }
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url=f'{API_URL}/sdapi/v1/txt2img', json=payload) as response:
+            if response.status != 200:
+                raise ValueError(f"Request failed with status {response.status}")
+            r = await response.json()
+
+    image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
+    image.save(output_image_path)
+    return output_image_path
+
+positive = 'illustration of a grassy field, masterpiece, trending on deviantart, (disney style:1.2), clouds'
+negative = 'frame, people, humans, characters'
+result = asyncio.run(generate_image_from_text('mountain-segmentation.png', 'controlnetoutput.png', positive, negative))
+print(result)
+
